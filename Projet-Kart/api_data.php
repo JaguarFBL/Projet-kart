fc95091b1d8672446901f7e76590213aaad877e0
JaguarFBL <louanrobertroussel2008@gmail.com>
2026-03-30 09:22:16 +0200

    Delete Projet-Kart/api_data.php

--- Projet-Kart/api_data.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'test.php';
-header('Content-Type: application/json');
-
-$stmt = $mysqlClient->prepare('SELECT * FROM capteur ORDER BY date DESC LIMIT 1');
-$stmt->execute();
-$derniere = $stmt->fetch(PDO::FETCH_ASSOC);
-
-echo json_encode($derniere);
